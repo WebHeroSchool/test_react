d145f06102e8a5a572efb6496a76c7b40613714e
KsuBurn <ksuhablondo@gmail.com>
2020-05-26 12:02:47 +0300

    test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-        >
-          Learn
-        </a>
-      </header>
+
+
+
+const App = () => (
+  <Router>
+    <div >
+      <a
+        href='https://webheroschool.ru/'
+        target='_blank'
+        rel='noopener noreferrer'
+
+      >
+        <div ></div>
+      </a>
+      <div >
+        <NavLink to='/todo' >
+          Дела
+        </NavLink>
+        <NavLink to='/' >
+          Обо мне
+        </NavLink>
+      </div>
+      <Route path='/' exact  />
+      <Route path='/todo'  />
     </div>
-  );
-}
+  </Router>
+);
+
 
 export default App;
